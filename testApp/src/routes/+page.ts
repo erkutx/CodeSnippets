b1013f8c2495bef95d16940725b6ce72a53b5abf
Erkut Cetiner <erkutcetiner0@gmail.com>
2024-03-25 18:02:50 -0400

    tsconfig error not resolved
    getting
    500
    Internal Error

---
+++ testApp/src/routes/+page.ts
@@ -0,0 +1,22 @@
+import type { PageLoad } from "./$types";
+
+export const load = (() => {
+    
+
+    return {
+        snippets: [
+            {
+				title: "Cooper Codes Snippet",
+				language: "html",
+				code: `<div>This is a div</div>`,
+				favorite: false
+			},
+			{
+				title: "Cool code snippet",
+				language: "typescript",
+				code: `console.log("Subscribe");`,
+				favorite: false
+			}
+        ]
+    }
+}) satisfies PageLoad; // PageLoad -> PageData (in our frontend)
